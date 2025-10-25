81cd9721ced6cdf35106365d9bb732edb811f396
Zonaldo7 <zycsteam@foxmail.com>
2025-10-25 10:37:15 -0500

    update default terminal size

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  -- add gruvbox
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+      vim.g.gruvbox_material_background = "medium" -- Options: 'hard', 'medium', 'soft'
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
